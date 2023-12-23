36b20b89e892b08c57d620bf9eb32daaf700059e
pixelsapphire <rubynaxela@gmail.com>
2023-12-23 03:28:26 +0100

    Add TextTransform#encodeAsHexBytes(int) and TextTransform#encodeAsPseudoHexBytes(int)

--- src/main/java/com/pixelsapphire/silicon/util/TextTransform.java
+++ src/main/java/com/pixelsapphire/silicon/util/TextTransform.java
@@ -2,6 +2,8 @@ package com.pixelsapphire.silicon.util;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.stream.Stream;
+
 /**
  * A utility class containing several methods for transforming text.
  */
@@ -15,7 +17,7 @@ public class TextTransform {
      */
     public static @NotNull String encodeAsHexBytes(@NotNull String text) {
         final StringBuilder builder = new StringBuilder();
-        for (byte b : text.getBytes()) builder.append(String.format("%02x", b));
+        builder.appendAll(text.getBytes(), b -> String.format("%02x", b));
         return builder.toString();
     }
 
@@ -28,9 +30,8 @@ public class TextTransform {
      */
     public static @NotNull String encodeAsPseudoHexBytes(@NotNull String text) {
         final StringBuilder builder = new StringBuilder();
-        for (byte b : text.getBytes())
-            for (char c : String.format("%02x", b).toCharArray())
-                builder.append(Character.isDigit(c) ? (char) (c + 17) : c);
+        for (final byte b : text.getBytes())
+            builder.appendAll(String.format("%02x", b).toCharArray(), c -> Character.isDigit(c) ? (char) (c + 17) : c);
         return builder.toString();
     }
 
@@ -42,5 +43,39 @@ public class TextTransform {
      */
     public static @NotNull String u2d(@NotNull String text) {
         return text.replace("_", "-");
+    }
+
+    /**
+     * Encodes an integer to a string. The integer is encoded as a sequence of
+     * bytes, each represented by two hex digits. Leading zero bytes are omitted,
+     * unless the integer is zero, in which case a single zero byte is encoded.
+     *
+     * @param value the integer to encode
+     * @return the encoded string
+     */
+    public static @NotNull String encodeAsHexBytes(int value) {
+        if (value == 0) return "00";
+        final StringBuilder encoded = new StringBuilder();
+        encoded.appendAll(Stream.of((byte) ((value >> 24) & 0xFF), (byte) ((value >> 16) & 0xFF), (byte) ((value >> 8) & 0xFF),
+                                    (byte) (value & 0xFF)).dropWhile((b) -> b == 0).map(b -> String.format("%02x", b)).toList());
+        return encoded.toString();
+    }
+
+    /**
+     * Encodes an integer to a string that contains only letters, a-f and A-K. This is done
+     * by encoding the integer as a sequence of bytes, each represented by two hex digits,
+     * and then replacing all base-10 digits with capital letters. Leading zero bytes are
+     * omitted, unless the integer is zero, in which case a single zero byte is encoded.
+     *
+     * @param value the integer to encode
+     * @return the encoded string
+     */
+    public static @NotNull String encodeAsPseudoHexBytes(int value) {
+        if (value == 0) return "AA";
+        final StringBuilder encoded = new StringBuilder();
+        for (final byte b : Stream.of((byte) ((value >> 24) & 0xFF), (byte) ((value >> 16) & 0xFF), (byte) ((value >> 8) & 0xFF),
+                                      (byte) (value & 0xFF)).dropWhile((b) -> b == 0).toList())
+            encoded.appendAll(String.format("%02x", b).toCharArray(), c -> Character.isDigit(c) ? (char) (c + 17) : c);
+        return encoded.toString();
     }
 }
--- src/test/java/com/pixelsapphire/silicon/test/TextTransformTest.java
+++ src/test/java/com/pixelsapphire/silicon/test/TextTransformTest.java
@@ -18,4 +18,20 @@ public class TextTransformTest {
         final var hex = TextTransform.encodeAsPseudoHexBytes("Hello, World!");
         assertEquals("EIGFGcGcGfCcCAFHGfHCGcGECB", hex);
     }
+
+    @Test
+    void testInt2Hex() {
+        var hex = TextTransform.encodeAsHexBytes(7);
+        assertEquals("07", hex);
+        hex = TextTransform.encodeAsHexBytes(0xF54AC8);
+        assertEquals("f54ac8", hex);
+    }
+
+    @Test
+    void testInt2PseudoHex() {
+        var hex = TextTransform.encodeAsPseudoHexBytes(7);
+        assertEquals("AH", hex);
+        hex = TextTransform.encodeAsPseudoHexBytes(0xF54AC8);
+        assertEquals("fFEacI", hex);
+    }
 }
