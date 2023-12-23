d05a5703c7f7d43bbd55a96b977c549985f679ba
pixelsapphire <rubynaxela@gmail.com>
2023-12-23 02:11:51 +0100

    Add SiCDCompiler#insertCode

--- src/main/java/com/pixelsapphire/silicon/sicd/compiler/SiCDCompiler.java
+++ src/main/java/com/pixelsapphire/silicon/sicd/compiler/SiCDCompiler.java
@@ -6,26 +6,32 @@ import com.pixelsapphire.silicon.sicd.parser.node.Node;
 import com.pixelsapphire.silicon.sicd.parser.node.RootNode;
 import com.pixelsapphire.silicon.sicd.parser.node.definition.ComponentDefinitionNode;
 import com.pixelsapphire.silicon.sicd.parser.node.definition.PointDefinitionNode;
+import com.pixelsapphire.silicon.util.StringBuilder;
 import org.jetbrains.annotations.NotNull;
 
 public class SiCDCompiler {
 
     private final RootNode circuit;
+    private final StringBuilder codeBuffer;
 
     public SiCDCompiler(@NotNull RootNode circuit) {
         this.circuit = circuit;
+        this.circuit.addCodeInsertionListener(this::insertCode);
+        codeBuffer = new StringBuilder();
+    }
+
+    public void insertCode(@NotNull String code) {
+        codeBuffer.appendln(code);
     }
 
     public @NotNull String compile() {
-        final StringBuilder code = new StringBuilder();
         for (final Node node : circuit.getNodes()) {
             if (node instanceof final ComponentDefinitionNode componentDefinition)
-                code.append(CommonNodes.compileComponent(componentDefinition, circuit));
+                codeBuffer.appendln(CommonNodes.compileComponent(componentDefinition, circuit));
             else if (node instanceof final PointDefinitionNode pointDefinition)
-                code.append(Point.compilePoint(pointDefinition, circuit));
+                codeBuffer.appendln(Point.compilePoint(pointDefinition, circuit));
             else throw new CompilationException("Unexpected node: " + node.getType(), node.getLocationOrUnknown());
-
         }
-        return code.toString();
+        return codeBuffer.toString();
     }
 }
